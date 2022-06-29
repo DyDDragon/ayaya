3e88f110fe319ffcaf9e5fe5c7c6d43bcc445252
DyDDragon <zozocreeper@gmail.com>
2022-06-30 01:23:06 +0200

    Ayaya

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Client } = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
-
+const dotenv = require('dotenv'); dotenv.config();
 const client = new Client({ intents: 1 });
+
+require('./utils/handlers/EventUtil')(client);
 
 client.once('ready', () => {
     console.log('Ready!');
 });   //send a log when connected
 
 client.login(process.env.TOKEN);
+
