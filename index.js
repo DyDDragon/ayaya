3da25802f6858623701e08449bea3c14acdda78f
DyDDragon <zozocreeper@gmail.com>
2022-06-30 01:25:05 +0200

    Aya

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ const client = new Client({ intents: 1 });
 
 require('./utils/handlers/EventUtil')(client);
 
-client.once('ready', () => {
-    console.log('Ready!');
-});   //send a log when connected
-
 client.login(process.env.TOKEN);
 
