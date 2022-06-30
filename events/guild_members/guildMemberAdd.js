935f9520bf163f89e3d31f6fd28dc385a6a0a3cd
DyDDragon <zozocreeper@gmail.com>
2022-06-30 13:49:21 +0200

    Update

--- events/guild_members/guildMemberAdd.js
+++ events/guild_members/guildMemberAdd.js
@@ -1,7 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: "guildMemberAdd",
     once: false,
     async execute(client, member) {
-        console.log('test');
-}
+        const embed = new MessageEmbed()
+            .setAuthor({name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL()})
+            .setColor('#21ff81')
+            .setDescription(`± Nom d'utilisateur : ${member}
+            ± Créé le : <t:${member.user.createdTimestamp}:f> (<t:${member.user.createdTimestamp}:R>)
+            ± Rejoint le : <t:${member.joinedTimestamp}:f> (<t:${member.joinedTimestamp}:R>)`)
+            .setFooter({ text: 'Le membre a rejoint le serveur' })
+            .setTimestamp();
+
+        const logChannel = client.channels.cache.get('627534874210861056');
+        logChannel.send({embeds: [embed]});
+    },
 };
