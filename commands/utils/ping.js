838870995fe29b3dada06861f608b0f96168a9d8
DyDDragon <zozocreeper@gmail.com>
2022-06-30 02:46:10 +0200

    Update

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: 'ping',
     description: 'Commande ping'
-    once: true,
+    once: false,
     run: (client, message, args) => {
         message.channel.send('Pong !');
     },
     runSlash: (client, interaction) => {
         interaction.reply('Pong !');
     }
-}
+};
