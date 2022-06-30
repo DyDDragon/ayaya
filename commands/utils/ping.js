feec5d074d2aec85bcc5530eb763ff46d81b590c
DyDDragon <zozocreeper@gmail.com>
2022-06-30 16:29:04 +0200

    Update

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,10 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'ping',
     description: 'Commande ping',
-    run(client, message, args) {
-        message.channel.send('Pong !');
-    },
     runInteraction(client, interaction) {
-        interaction.reply('Pong !');
+        const embed = new MessageEmbed()
+            .setTitle('Pong !')
+            .setThumbnail(client.user.displayAvatarURL())
+            .addFields(
+                {   name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true},
+                //{   name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp) /1000}:R>`, inline: true},
+            )
+            .setTimestamp()
+            .setFooter({ text: interaction.user.username, icon_url: interaction.user.displayAvatarURL() });
+        interaction.reply({ embeds: [embed]});
     }
 };
