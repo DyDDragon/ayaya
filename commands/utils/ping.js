baed7cb008d9debf1dae0b4bdbd7c1109fcd3ade
DyDDragon <zozocreeper@gmail.com>
2022-06-30 16:30:58 +0200

    Update

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -9,7 +9,7 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
                 {   name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true},
-                //{   name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp) /1000}:R>`, inline: true},
+                {   name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp /1000)}:R>`, inline: true},
             )
             .setTimestamp()
             .setFooter({ text: interaction.user.username, icon_url: interaction.user.displayAvatarURL() });
