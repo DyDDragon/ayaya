34f210ab2dc99bb0d0d93fda7f4cfcc530f024bd
DyDDragon <zozocreeper@gmail.com>
2022-06-30 14:02:50 +0200

    Update

--- events/guild_members/guildMemberAdd.js
+++ events/guild_members/guildMemberAdd.js
@@ -1,15 +1,21 @@
-const { MessageEmbed } = require('discord.js');
+const dayjs = require('dayjs');
+const { MessageEmbed, Formatters } = require('discord.js');
 
 module.exports = {
     name: "guildMemberAdd",
     once: false,
     async execute(client, member) {
+        const creationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+        const relativeCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+        const joinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+        const relativeJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+
         const embed = new MessageEmbed()
             .setAuthor({name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL()})
             .setColor('#21ff81')
             .setDescription(`± Nom d'utilisateur : ${member}
-            ± Créé le : <t:${member.user.createdTimestamp /1000}:f> (<t:${member.user.createdTimestamp /1000}:R>)
-            ± Rejoint le : <t:${member.joinedTimestamp /1000}:f> (<t:${member.joinedTimestamp /1000}:R>)`)
+            ± Créé le : ${creationTimestamp}> (${relativeCreationTimestamp})
+            ± Rejoint le : ${joinTimestamp} (${relativeJoinTimestamp})`)
             .setFooter({ text: 'Le membre a rejoint le serveur' })
             .setTimestamp();
 
