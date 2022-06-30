543df212c1af2cfe9d8a440e172b4a73b8296bae
DyDDragon <zozocreeper@gmail.com>
2022-07-01 00:46:52 +0200

    Update

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -4,19 +4,18 @@ module.exports = {
     name: 'ping',
     description: 'Commande ping',
     async runInteraction(client, interaction) {
-        const tryPing = await MessageEmbed.channel.send("Ping ?");
+        const tryPing = await interaction.reply({content : "Ping ?", fetchReply: true});
 
         const embed = new MessageEmbed()
             .setTitle('Pong !')
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
                 {   name: 'Latence API', value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true},
-                {   name: 'Latence BOT', value: `\`\`\`${tryPing.createdTimestamp - message.createdTimestamp}ms\`\`\``, inline: true},
+                {   name: 'Latence BOT', value: `\`\`\`${tryPing.createdTimestamp - interaction.createdTimestamp}ms\`\`\``, inline: true},
                 //{   name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp /1000)}:R>`},
             )
             .setTimestamp()
             .setFooter({ text: interaction.user.username, icon_url: interaction.user.displayAvatarURL() });
-        //interaction.reply({ embeds: [embed]});
-        tryPing.edit({ content: '', embeds: [embed]});
+        interaction.editReply({ content: null, embeds: [embed]});
     }
 };
