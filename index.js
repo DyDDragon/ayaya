75d536be586b93b381255e8c65d9715757f466d3
DyDDragon <zozocreeper@gmail.com>
2022-06-30 02:39:23 +0200

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.commands = new Collection();
 ['CommandUtil', 'EventUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client) });
 
 process.on('exit', code => { console.log(`Exiting with code ${code}`) });
-process.on('uncaughtException', (err, origin => { console.log(`Uncaught exception: ${err}`, `Origin: ${origin}`) }));
+process.on('uncaughtException', (err, origin) => { console.log(`Uncaught exception: ${err}`, `Origin: ${origin}`) });
 process.on('unhandledRejection', (reason, promise) => { console.log(`Unhandled rejection: ${reason}\n-----\n`, promise) });
 process.on('warning', (...args) => console.log(...args));
 
